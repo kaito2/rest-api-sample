1751fcd857a484e9bd24a9fd24e1ff633618b85c
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-05-04 22:42:04 +0900

    add nested span

--- echo_service.go
+++ echo_service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/trace"
+	"time"
 
 	echoservice "github.com/kaito2/rest-api-sample/gen/echo_service"
 	log "github.com/kaito2/rest-api-sample/gen/log"
@@ -12,27 +14,43 @@ import (
 // echo-service service example implementation.
 // The example methods log the requests and return zero values.
 type echoServicesrvc struct {
+	tracer trace.Tracer
 	logger *log.Logger
+	projectID string
 }
 
 // NewEchoService returns the echo-service service implementation.
 func NewEchoService(logger *log.Logger) echoservice.Service {
-	return &echoServicesrvc{logger}
+	projectID := "kaito2"
+	tracer := global.TraceProvider().Tracer("/echo-get")
+	return &echoServicesrvc{logger: logger, tracer: tracer, projectID: projectID}
 }
 
 // EchoGet implements echo-get.
 func (s *echoServicesrvc) EchoGet(ctx context.Context, p *echoservice.EchoGetPayload) (res string, err error) {
 	// Create custom span.
-	tracer := global.TraceProvider().Tracer("/echo-get")
-	_, span := tracer.Start(ctx, "sample span")
+	_, span := s.tracer.Start(ctx, "sample_span1")
 	defer span.End()
 
-	projectID := "kaito2"
 	s.logger.Info().Fields(map[string]interface{}{
-		"message": "hoge",
-		"severity": "warn",
-		"logging.googleapis.com/trace": fmt.Sprintf("projects/%s/traces/%s", projectID, span.SpanContext().TraceIDString()),
+		"message": "message of sample span1",
+		"severity": "info",
+		"logging.googleapis.com/trace": fmt.Sprintf("projects/%s/traces/%s", s.projectID, span.SpanContext().TraceIDString()),
 		"logging.googleapis.com/spanId": span.SpanContext().SpanIDString(),
 	}).Send()
+
+	time.Sleep(300*time.Millisecond)
 	return "sample response", nil
+}
+
+func (s *echoServicesrvc) sampleFunction(ctx context.Context) {
+	_, span := s.tracer.Start(ctx, "sample_span2")
+	defer span.End()
+	s.logger.Info().Fields(map[string]interface{}{
+		"message": "message of sample span2",
+		"severity": "info",
+		"logging.googleapis.com/trace": fmt.Sprintf("projects/%s/traces/%s", s.projectID, span.SpanContext().TraceIDString()),
+		"logging.googleapis.com/spanId": span.SpanContext().SpanIDString(),
+	}).Send()
+	time.Sleep(500*time.Millisecond)
 }
